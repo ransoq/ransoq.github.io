ea63cdae0f09f5ef5bc1ab8389eb18405563e400
Maxim <maximransov@gmail.com>
2020-08-13 01:22:10 +0300

    add validate

--- pulsometr/src/js/script.js
+++ pulsometr/src/js/script.js
@@ -46,13 +46,45 @@ document.querySelector('.next').addEventListener('click', function () {
     $('.modal__close').on('click', function () {
       $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
     });
-    
+
     $('.button_mini').each(function(i) {
       $(this).on('click', function() {
         $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
         $('.overlay, #order').fadeIn('slow');
       })
     });
+
+    // Validate
+    function validateForms(form) {
+      $(form).validate({
+        rules: {
+          name: {
+            required: true,
+            minlength: 2
+          },
+          phone: "required",
+          email: {
+            required: true,
+            email: true
+          }
+        },
+        messages: {
+          name: {
+            required: "Пожалуйста, введите своё имя",
+            minlength: jQuery.validator.format("Введите минимум {0} символа")
+          },
+          phone: "Пожалуйста, введите свой номер телефона",
+          email: {
+            required: "Пожалуйста, введите свою почту",
+            email: "Неправильно введён адрес почты"
+          }
+        }
+      });
+    };
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
 
   });
 })(jQuery);
