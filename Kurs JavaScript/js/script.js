7f35f0e153b795bf059ce776c84ba681c055d707
Maxim <maximransov@gmail.com>
2020-08-23 17:32:38 +0300

    fix pulsometr

--- Kurs JavaScript/js/script.js
+++ Kurs JavaScript/js/script.js
@@ -39,7 +39,7 @@ function rememberMyFilms() {
     }
 }
 
-// rememberMyFilms();
+rememberMyFilms();
 
 function detectPersonalLevel() {
     if (personalMovieDB.count < 10) {
@@ -53,7 +53,7 @@ function detectPersonalLevel() {
     }
 }
 
-// detectPersonalLevel();
+detectPersonalLevel();
 
 function showMyDB(hidden) {
     if (!hidden) {
@@ -71,24 +71,95 @@ function writeYourGenres() {
 
 writeYourGenres();
 
-// ----------------------- Циклы ----------------------- //
+// CallBack //
 
-// let num = 50;
+function first() {
+    // Do something 
+    setTimeout(function() {
+        console.log(1);
+    }, 500);
+}
 
-// while (num < 55) {
-//     console.log(num);
-//     num++;
-// }
+function second() {
+    console.log(2);
+}
 
-// do {
-//     console.log(num);
-//     num++;
-// }
-// while (num < 55);
+first();
+second();
 
-// for (let i = 1; i < 10; i++) {
-//     if (i === ) {
-//         break;
+function learnJS(lang, callback) {
+    console.log(`Я учу: ${lang}`);
+    callback();
+}
+
+function done() {
+    console.log('Я прошёл этот урок!');
+}
+
+learnJS('JavaScript', done);
+
+// Объекты //
+
+// const options = {
+//     name: 'test',
+//     width: 1024,
+//     height: 1024,
+//     colors: {
+//         border: 'black',
+//         bg: 'red'
+//     },
+//     makeTest: function() {
+//         console.log("Test");
 //     }
-//     console.log(i)
+// };
+
+// options.makeTest();
+
+// const {border, bg} = options.colors;                                                 // -------- Деструктуризация //
+// console.log(border);
+
+// console.log(Object.keys(options).length);                                         // -------- Перебор количества параметров в обеъкте при помощи массива //
+
+// delete options.name;
+
+// console.log(options);
+
+// let counter = 0;
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for(let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`); 
+//             counter++;
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//         counter++;
+//     }
 // }
+// console.log(counter);
+
+// --- Массивы и Псевдо-массивы --- //
+
+// const arr = [2, 13, 26, 8, 10];
+// arr.sort(compareNum);
+// console.log(arr);
+
+// function compareNum(a, b) {
+//     return a - b;
+// }
+
+// arr.forEach(function(item, i, arr) {                                                    // --- Перебор массива forEach с callback --- //
+//     console.log(`${i}: ${item} внутри массива ${arr}`);
+// });
+
+// arr.pop();
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// const str = prompt("", "");
+// const products = str.split(", ");
+// products.sort();
+// console.log(products.join('; '));
