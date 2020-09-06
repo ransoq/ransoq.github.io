2a87741c79b3ab8214adbc48ecd7cbe61657169f
Maxim <maximransov@gmail.com>
2020-09-07 01:51:47 +0300

    Add Timer (Food)

--- Test/js/script.js
+++ Test/js/script.js
@@ -1,2 +1,28 @@
 "use strict";
 
+// const now = new Date('2020-09-04');
+// new Date.parse('2020-09-04');
+
+// console.log(now.setHours(18, 40));
+// console.log(now);
+
+
+
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getDay());
+// console.log(now.getUTCHours());
+
+// console.log(now.getTimezoneOffset());
+// console.log(now.getTime());
+
+let start = new Date();
+
+for (let i = 0; i < 100000; i++) {
+    let some = i ** 3;
+}
+
+let end = new Date();
+
+alert(`Цикл отработал за ${end - start} мс`);
