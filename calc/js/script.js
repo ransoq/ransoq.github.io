704f8cef136e43949f6b729df37d119bc94c7be9
ransoq <68750711+ransoq@users.noreply.github.com>
2020-09-13 22:14:33 +0300

    Test
    test

---
+++ calc/js/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const inputRub = document.querySelector('#rub'),
+      inputUsd = document.querySelector('#usd');
+
+inputRub.addEventListener('input', () => {
+    const request = new XMLHttpRequest();
+
+    request.open('GET', 'js/current.json'); // method, url, async, login, pass
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
+    
+    request.addEventListener('load', () => {
+        if (request.status === 200) {
+            console.log(request.response);
+
+            const data = JSON.parse(request.response);
+            inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+        } else {
+            inputUsd.value = 'Что-то пошло не так';
+        }   
+    });
+
+    // status (404 not fund, 0, 200 etc);
+    // statusText 
+    // response 
+    // readyState - состояние запроса (4 DONE)
+});
