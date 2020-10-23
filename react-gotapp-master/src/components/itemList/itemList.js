941c8cfae50e497c78ae84b5b07a87a3dfa65f6e
Maxim <maximransov@gmail.com>
2020-10-23 11:35:20 +0300

    new step (module)

--- react-gotapp-master/src/components/itemList/itemList.js
+++ react-gotapp-master/src/components/itemList/itemList.js
@@ -1,69 +1,99 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import './itemList.css';
 import Spinner from '../spinner';
-import ErrorMessage from '../error';
+// import ErrorMessage from '../error';
+// import PropTypes from 'prop-types';
+// import gotService from '../../services/gotService';
 
-export default class ItemList extends Component {
-   
-    state = {
-        itemList: null,
-        error: false
-    }
+function ItemList ({getData, onItemSelected, renderItem}) {
 
-    componentDidMount() {
-        const {getData} = this.props;
+    const [itemList, updateList] = useState([]);
 
+    useEffect(() => {
         getData()
-            .then( (itemList) => {
-                this.setState({
-                    itemList,
-                    error: false
-                })
+            .then( (data) => {
+                updateList(data);
             })
-            .catch(() => {this.onError()});
-    }
+            // .catch(() => {onError()});
+    }, [])
 
-    onError(status){
-        this.setState({
-            itemList: null,
-            error: true
-        })
-    }
+    // function onError(){
+    //     this.setState({
+    //         itemList: null,
+    //         error: true
+    //     })
+    // }
 
-    renderItems(arr) {
+    function renderItems(arr) {
         return arr.map((item) => {
             const {id} = item;
-            const label = this.props.renderItem(item);
+            const label = renderItem(item);
 
             return (
                 <li 
                     key={id}
                     className="list-group-item"
-                    onClick={() => this.props.onItemSelected(id)}>
+                    onClick={() => onItemSelected(id)}>
                     {label}
                 </li>
             )
         });
     }
 
-    render() {
-
-        const {itemList, error} = this.state;
-
-        if(error){
-            return <ErrorMessage/>
-        }
-
-        if(!itemList) {
-            return <Spinner/>
-        }
-
-        const items = this.renderItems(itemList);
-
-        return (
-            <ul className="item-list list-group">
-                {items}
-            </ul>
-        );
+    if(!itemList) {
+        return <Spinner/>
     }
+
+    const items = renderItems(itemList);
+
+    return (
+        <ul className="item-list list-group">
+            {items}
+        </ul>
+    );
 }
+
+export default ItemList;
+
+// Компоненты высшего порядка
+
+// const withData = (View, getData) => {
+//     return class extends Component {
+
+//         state = {
+//             data: null,
+//             error: false
+//         }
+    
+//         static propTypes = {
+//             onItemSelected: PropTypes.func
+//         }
+    
+//         componentDidMount() {
+//             getData()
+//                 .then( (data) => {
+//                     this.setState({
+//                         data,
+//                         error: false
+//                     })
+//                 })
+//                 .catch(() => {this.onError()});
+//         }
+        
+//         render() {
+//             const {data, error} = this.state;
+
+//             if(error){
+//                 return <ErrorMessage/>
+//             }
+    
+//             if(!data) {
+//                 return <Spinner/>
+//             }
+
+//             return <View {...this.props} data={data}/>
+//         }
+//     }
+// }
+// const {getAllCharacters} = new gotService();
+// export default withData(ItemList, getAllCharacters);
